b07e16033ef3b206439f00f585a20c7d530aa3e7
bbrtj <bbrtj.pro@gmail.com>
2024-10-11 16:40:22 +0200

    Initial - mint dist with dzil

---
+++ lib/Storage/Abstract.pm
@@ -0,0 +1,38 @@
+package Storage::Abstract;
+
+use v5.14;
+use warnings;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Storage::Abstract - New module
+
+=head1 SYNOPSIS
+
+	use Storage::Abstract;
+
+	# do something
+
+=head1 DESCRIPTION
+
+This module lets you blah blah blah.
+
+=head1 SEE ALSO
+
+L<Some::Module>
+
+=head1 AUTHOR
+
+Bartosz Jarzyna E<lt>bbrtj.pro@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2024 by Bartosz Jarzyna
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
